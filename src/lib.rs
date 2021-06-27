c634e61dc22a73e31abad26881678159ad771cec
VisualBasist <VisualBasist@users.noreply.github.com>
2021-06-27 17:50:01 +0900

    基本のプロフィール

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,31 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+struct Name {}
+
+struct FullName {
+    last_name: Name,
+    first_name: Name,
+}
+
+struct BirthDay {
+    month: u8,
+    day: u8,
+}
+
+enum School {}
+enum Club {}
+
+pub struct Student {
+    school: School,
+    club: Club,
+    name: FullName,
+
+    // 永遠の・・・?
+    age: u32,
+
+    birthday: BirthDay,
+    height: f64,
+    hobby: str,
+}
