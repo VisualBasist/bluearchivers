90a4375ccbc98719c754204ec30d28361ee0bcf7
VisualBasist <VisualBasist@users.noreply.github.com>
2021-07-04 16:20:02 +0900

    AP買うテストコードとそのための実装かいた

--- src/currency.rs
+++ src/currency.rs
@@ -5,6 +5,15 @@ struct AP {
 }
 
 impl AP {
+    pub fn new(value: u32, last_recoverd_time: std::time::Instant) -> AP {
+        AP {
+            value,
+            last_recoverd_time,
+        }
+    }
+    pub fn get(&self) -> u32 {
+        self.value
+    }
     pub fn buy_120(
         &mut self,
         blue_pyroxene: &mut BluePyroxene,
@@ -19,11 +28,32 @@ impl AP {
 
 struct Credit(u32);
 struct BluePyroxene(u32);
+#[derive(Debug)]
 struct NotEnoughBluePyroxene;
 
 impl BluePyroxene {
+    fn new(value: u32) -> BluePyroxene {
+        BluePyroxene(value)
+    }
+    fn get(&self) -> u32 {
+        self.0
+    }
     fn consume(&mut self, amount: u32) -> Result<(), NotEnoughBluePyroxene> {
         self.0 = self.0.checked_sub(amount).ok_or(NotEnoughBluePyroxene)?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::currency;
+
+    #[test]
+    fn buy_AP() {
+        let mut ap = currency::AP::new(0, std::time::Instant::now());
+        let mut blue_pyroxene = currency::BluePyroxene::new(30);
+        ap.buy_120(&mut blue_pyroxene).unwrap();
+        assert_eq!(ap.get(), 120);
+        assert_eq!(blue_pyroxene.get(), 0);
     }
 }
